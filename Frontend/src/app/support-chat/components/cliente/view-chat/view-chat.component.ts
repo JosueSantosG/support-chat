78084b6084e8a5497c4879e2628588e26e22a2c4
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-07-31 00:50:06 -0500

    chore: Add support chat view component for client

---
+++ Frontend/src/app/support-chat/components/cliente/view-chat/view-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-chat',
+  templateUrl: './view-chat.component.html',
+  styleUrl: './view-chat.component.css'
+})
+export class ViewChatComponent {
+
+}
