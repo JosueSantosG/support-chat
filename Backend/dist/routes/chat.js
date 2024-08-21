7b762ab8dd71ba9b918784eeae29fcb82ad6d6b9
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-08-20 21:27:54 -0500

    Revert "chore: Update dist"
    This reverts commit 76e920ea14f3863ba93a7c5a7eb588cfd12251d5.

--- Backend/dist/routes/chat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const chat_1 = require("../controllers/chat");
-const router = (0, express_1.Router)();
-/* router.get('/', getMessages); */
-router.post('/createUser', chat_1.createUser);
-exports.default = router;
-//# sourceMappingURL=chat.js.map
--- Backend/dist/routes/waitingRoom.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const waitingRoom_1 = require("../controllers/waitingRoom");
-const router = (0, express_1.Router)();
-router.get('/listUsers', waitingRoom_1.waitingRoom);
-exports.default = router;
-//# sourceMappingURL=waitingRoom.js.map
