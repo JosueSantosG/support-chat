b812f986361b17d0a0a689eda5935f014c71d9e0
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-08-21 22:37:56 -0500

    chore: Update .gitignore and environment file

---
+++ Frontend/dist/frontend/browser/chunk-V6F2ZFDI.js
@@ -0,0 +1,1 @@
+import{k as a}from"./chunk-NYJXWSWA.js";export{a as SupportChatModule};
--- Frontend/src/environments/environments.ts
+++ Frontend/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const env = {
-    production: false,
-    url: 'http://localhost:3333'
+    production: true,
+    url: 'https://chat-socketio-cef820fa128c.herokuapp.com'
 };
