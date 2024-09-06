6fad4ae0088e1b68f6c4fd90ac681cd2d7f692c3
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-09-06 18:36:26 -0500

    Updated env

--- Frontend/src/environments/environments.ts
+++ Frontend/src/environments/environments.ts
@@ -1,11 +1,11 @@
-/* export const env = {
+export const env = {
     production: true,
     url: 'https://chat-socketio-cef820fa128c.herokuapp.com'
-}; */
+};
 
-export const env = {
+/* export const env = {
     production: false,
     url: 'http://localhost:3333'
 };
-
+ */
 
