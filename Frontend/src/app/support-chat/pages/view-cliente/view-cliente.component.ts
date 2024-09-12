c6242acc45dc43447761ad529d764e571ed7d68a
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-09-12 01:33:39 -0500

    feat: Add notification

--- Frontend/src/app/support-chat/pages/view-cliente/view-cliente.component.ts
+++ Frontend/src/app/support-chat/pages/view-cliente/view-cliente.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-cliente',
   templateUrl: './view-cliente.component.html',
-  styleUrl: './view-cliente.component.css'
+  styleUrls: ['./view-cliente.component.css']
 })
-export class ViewClienteComponent {
+export class ViewClienteComponent implements OnInit {
+  showNotification = false; // Inicialmente la notificación está oculta
+  isHidden = true; // Controla la animación de entrada y salida
+  
 
+  ngOnInit() {
+    // Retraso de 1 segundo antes de mostrar la notificación
+    setTimeout(() => {
+      this.showNotification = true; // Mostrar la notificación
+      setTimeout(() => {
+        this.isHidden = false; // Aplicar animación de entrada
+      }, 50); // Retraso para permitir el renderizado antes de la animación
+    }, 1000); // Retraso de 1 segundo
+  }
+
+  closeNotification() {
+    this.isHidden = true; // Aplicar animación de salida
+    setTimeout(() => {
+      this.showNotification = false; // Ocultar la notificación después de la animación de cierre
+    }, 500); // Duración de la animación de salida
+  }
 }
